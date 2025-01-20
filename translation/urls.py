abc9bc410fe37201dab988846f70bc77cce1ea12
asakaicode <tubakaito5100@gmail.com>
2025-01-21 02:09:42 +0900

    feat: create simple views

--- translation/urls.py
+++ translation/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'translation'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:translation_id>/', views.detail, name='detail')
 ]
--- translation/views.py
+++ translation/views.py
@@ -1,5 +1,18 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from translation.models import Translation
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    translations = Translation.objects.all()
+    context = {
+        'translations': translations,
+    }
+
+    return render(request, 'translation/index.html', context)
+
+
+def detail(request, translation_id):
+    translation = get_object_or_404(Translation, pk=translation_id)
+
+    return render(request, "translation/detail.html", {"translation": translation})
